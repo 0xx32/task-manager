4891cfce4beac8fd4dc17da91f3f0e503847ec7b
0x32 <dimas1523@mail.ru>
2026-01-29 23:13:09 +0300

    first commit 2

---
+++ eslint.config.js
@@ -0,0 +1,83 @@
+import antfu from '@antfu/eslint-config'
+import perfectionist from 'eslint-plugin-perfectionist'
+
+export default antfu(
+	{
+		vue: true,
+		formatters: false,
+		typescript: true,
+		stylistic: false,
+	},
+	{
+		rules: {
+			'vue/multiline-html-element-content-newline': 'off',
+		},
+	},
+	{
+		name: '0x32/sort',
+		plugins: {
+			perfectionist,
+		},
+		rules: {
+			'perfectionist/sort-array-includes': [
+				'error',
+				{
+					order: 'asc',
+					type: 'alphabetical',
+				},
+			],
+
+			'perfectionist/sort-imports': [
+				'error',
+				{
+					groups: [
+						'type',
+						['value-builtin', 'value-external'],
+						'type-internal',
+						'value-internal',
+						['type-parent', 'type-sibling', 'type-index'],
+						['value-parent', 'value-sibling', 'value-index'],
+						'side-effect-style',
+						'unknown',
+					],
+					internalPattern: ['^~/.*', '^@/.*'],
+					newlinesBetween: 1,
+					order: 'asc',
+					type: 'natural',
+				},
+			],
+
+			'perfectionist/sort-interfaces': [
+				'error',
+				{
+					groups: ['unknown', 'method', 'multiline'],
+					order: 'asc',
+					type: 'alphabetical',
+				},
+			],
+
+			'perfectionist/sort-union-types': [
+				'error',
+				{
+					groups: [
+						'conditional',
+						'function',
+						'import',
+						'intersection',
+						'keyword',
+						'literal',
+						'named',
+						'object',
+						'operator',
+						'tuple',
+						'union',
+						'nullish',
+					],
+					order: 'asc',
+					specialCharacters: 'keep',
+					type: 'alphabetical',
+				},
+			],
+		},
+	}
+)
---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>f</template>
+
+<style scoped></style>
